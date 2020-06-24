7d8e4b3f5e4075e12a9478f64f5757c991d0224a
MHWauben <martine.wauben@gmail.com>
2020-06-24 13:41:05 +0100

    Fix date column bug

--- functions/results_prep.R
+++ functions/results_prep.R
@@ -5,6 +5,7 @@ prep_country <- function(country){
 prep_results <- function(q_list){
   out <- read.csv(q_list[[1]], stringsAsFactors = FALSE)
   if(nrow(out) > 0){
+    out$Date <- out[, 1]
     out$query <- q_list[[2]]
     out$Series <- prep_country(out$Series)
     return(out)
@@ -13,9 +14,9 @@ prep_results <- function(q_list){
 
 covid_results <- function(){
   query <- prep_api(prep_query(covid_query))
-  covid_only <- prep_results(list(query, 'all_covid')) %>%
-    dplyr::transmute(Date, 
-                     Series = prep_country(Series),
-                     all_covid = Value)
-  return(covid_only)
+  covid_only <- prep_results(list(query, 'all_covid')) 
+  covid_out <- data.frame(Date = covid_only[,1],
+                          Series = prep_country(covid_only$Series),
+                          all_covid = covid_only$Value)
+  return(covid_out)
 }
